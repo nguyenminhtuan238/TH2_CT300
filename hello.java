aeb0560c67bab5d49c55f066c0f625d50f211b00
NguyenMinhTuan <minhdu238@gmail.com>
2023-02-22 10:35:40 +0700

    Hello World!!

--- hello.java
+++ hello.java
@@ -1,6 +1,5 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
-		System.out.println("World!");
+        System.out.println("Hello World!");
     }
 }
